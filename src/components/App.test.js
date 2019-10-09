5323e5f9201028015338e7eacb1fb0c21fe62c89
Yoni <yoniakabecky@gmail.com>
2019-10-09 10:18:19 -0700

    fixed file path

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
-import rootReducer from './reducers/rootReducer';
+import rootReducer from '../reducers/rootReducer';
 import thunk from 'redux-thunk';
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
