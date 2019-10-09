bade910afe9241510fcead85a94b5b496b2485b2
Yoni <yoniakabecky@gmail.com>
2019-10-09 10:20:03 -0700

    fixed error

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
 import rootReducer from '../reducers/rootReducer';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
