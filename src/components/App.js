5780c95b623ea76eddebb0532e53e5ba07e41c36
Yoni <yoniakabecky@gmail.com>
2019-10-09 10:08:21 -0700

    fake error

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Navbar from './layouts/Navbar';
 import IssuesList from './issues/IssuesList';
 
+
 function App() {
   return (
     <div className="App">
       <Navbar />
       <IssuesList />
+      <Test />
     </div>
   );
 }
