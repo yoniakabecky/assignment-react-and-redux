54702ce0db26a09ef2f7b3775d44bb447ab04a76
Yoni <yoniakabecky@gmail.com>
2019-10-09 10:16:27 -0700

    added a test file

---
+++ src/components/App.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers/rootReducer';
+import thunk from 'redux-thunk';
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(
+        <Provider store={store}>
+            <App />
+        </Provider>,
+        div);
+    ReactDOM.unmountComponentAtNode(div);
+});
